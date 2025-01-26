fce262ea6289d18c65cf2140ceb6be3b7ad695d1
Koyo126 <168168540+Koyo126@users.noreply.github.com>
2025-01-26 20:57:57 +0900

    new file:   f90nml/Input.nml 	new file:   f90nml/README.md 	new file:   f90nml/RewrittenInput.nml 	new file:   f90nml/remake.py
      -> f90nml の使い方。

---
+++ f90nml/README.md
@@ -0,0 +1,11 @@
+# 概要
+
+Fortran の namelist file を、 python のライブラリである f90nml 
+を用いて読む・変数を変更して出力する例。
+
+> [!NOTE]
+> 本ファイルは Markdown のフォーマットに従っています。
+
+# Reference
+[1] https://f90nml.readthedocs.io/en/latest/
+
---
+++ f90nml/remake.py
@@ -0,0 +1,20 @@
+import f90nml
+import warnings
+
+with warnings.catch_warnings():
+    warnings.simplefilter('error')
+    try:
+        # ネームリストファイルを読み込む
+        nml = f90nml.read('./Input.nml')
+
+        # 変数を変更する
+        nml['ave']['nfile'] = 1
+        nml['ave']['avetype'] = 0
+        nml['initial']['start'] = True
+
+        # 変更したネームリストをファイルに書き込む
+        nml.write('./RewrittenInput.nml', force=True)
+
+    except Exception as e:
+        print(f"エラーまたは警告が発生しました: {e}")
+        exit(1)
